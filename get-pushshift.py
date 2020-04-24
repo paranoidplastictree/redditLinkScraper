f8b51f3f494af52aac91ef1bef9d9e7317633e73
paranoidplastictree <timstorms@gmail.com>
2020-04-23 23:39:33 -0400

    request all the mynoise posts

--- get-pushshift.py
+++ get-pushshift.py
@@ -7,56 +7,65 @@ import datetime
 from datetime import timedelta
 
 firstPostCreatedUtc = 1407254907 # 08/05/2014 @ 4:08pm (UTC)
+monthSeconds = 2678400
+timeSpanPerRequest = 12 * monthSeconds
 
-def appendToFile(fileNamePreFix, data):
-    fileName = fileNamePreFix + "_reddit-posts.txt"
+def appendToFile(startUtc, endUtc, data):
+    fileNameFormat = "{}_{}.json"
+    fileName = fileNameFormat.format(startUtc, endUtc)
     f = open(fileName, "a")
     f.write(data)
     f.close()
 
 def getUrl(before, after):
     subreddit = 'mynoise'
+    size = 1000
     sort = 'desc'
     sortType = 'created_utc'
-    fields = ['id', 'title', 'full_link', 'selftext', 'author', 'created_utc']
+    fields = ['id', 'title', 'full_link', 'selftext', 'author', 'created_utc', 'is_self']
     fields = ",".join(map(str,fields))
-    pushshiftUrl = 'https://api.pushshift.io/reddit/search/submission/?subreddit={}&sort={}&sort_type={}&after={}&before={}&fields={}'
-    return pushshiftUrl.format(subreddit, sort, sortType, after, before, fields)
+    pushshiftUrl = 'https://api.pushshift.io/reddit/search/submission/?subreddit={}&sort={}&sort_type={}&after={}&before={}&fields={}&size={}'
+    return pushshiftUrl.format(subreddit, sort, sortType, after, before, fields, size)
 
-def getPushshiftData():
-    now = int(time.time())
-    monthSeconds = 2678400
-    lastUtc = now - 3 * monthSeconds # get ~3 months as a test
-    # lastUtc = firstPostCreatedUtc
-    startUtc = now
-    index = 0
-    fileCount = 0
+def getPushshiftData(start):
+    totalSubs = 0
+    # lastUtc = now - 6 * monthSeconds # get ~6 months as a test
+    lastUtc = firstPostCreatedUtc
+    startUtc = start
+
     while startUtc >= lastUtc:
-        endUtc = startUtc - monthSeconds
+        subsFound = 0
+        endUtc = startUtc - timeSpanPerRequest
         url = getUrl(startUtc, endUtc)
         print(url)
         r = requests.get(url)
         data = json.loads(r.text)
 
-        if len(data['data']) == 0:
+        subsFound = len(data['data'])
+        totalSubs += subsFound
+        endDate = datetime.datetime.utcfromtimestamp(endUtc)
+        formattedEndDate = endDate.strftime("%h %d, %Y %H:%M")
+        print(str(subsFound) + " results found, last post dated: " + formattedEndDate)
+
+        if subsFound == 0:
             print('no data found')
             continue
 
-        if index % 4 == 0:
-            fileCount += 1
+        appendToFile(startUtc, endUtc, r.text)
 
-        appendToFile(str(fileCount), r.text)
         lastCreatedUtc = data['data'][-1]["created_utc"]
-
         if lastCreatedUtc == firstPostCreatedUtc:
             break
 
         print('saved to endUtc: ' + str(endUtc))
-        startUtc = endUtc - 1
-        index += 1
+        startUtc = endUtc
+    
+    print(str(totalSubs) + " total subs found")
     
 def main():
-    getPushshiftData()
+    startSeconds = int(time.time())
+    # startSeconds = 1426993907 # periodically, script fails. It may be a restriction on requests/minute
+    getPushshiftData(startSeconds)
     print('fin')
 
 def testMain():
@@ -66,10 +75,3 @@ def testMain():
     print(url)
 
 main()
-
-def testy():
-    x = 0
-    while x < 20:
-        x += 1
-        if x % 2 == 0:
-            print(x)
